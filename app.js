01e6f4ba6f4ab747b84f072c752c923e7887c6c9
Pomidoroff-Komrad <nesterov.zi@yandex.ru>
2024-05-01 19:22:05 +0300

    new middlewares

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js 
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -7,8 +6,10 @@ const gamesRouter = require('./routes/games');
 
 const PORT = 3000;
 const app = express();
+const corse = require("./middlewares/corse")
 
 app.use(
+    corse,
     bodyParser.json(),
     express.static(path.join(__dirname, 'public')),
     mainRoute,
